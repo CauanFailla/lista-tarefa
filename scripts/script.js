995901960ff294da94856a6a6485aab7db9c220e
cauanfailla <101191360+CauanFailla@users.noreply.github.com>
2023-06-08 18:07:20 -0300

    Novas funcionalidades

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,34 @@ const btn_back = document.querySelector('.btn_back')
 const btn_generate = document.querySelector('.btn_criar')
 const btn_confirmar = document.querySelector('.btn_confirmar')
 const btn_cont = document.querySelector('.btn_contin')
+const btn_historico =  document.querySelector('.btn-historico')
+const btn_home = document.querySelector('.btn-home')
 const sec_perso = document.querySelector('.sec_personalizacao')
 const list_task = document.querySelector('.list_task')
 const vazio  = document.querySelector('.men_vaz')
+const div_historico = document.querySelector('.historico')
+const main = document.querySelector('main')
 let taskName = document.querySelector('#itaskName')
 let teste = document.querySelector('.teste')
 const box_error = document.querySelector('.b_e')
+let  bd = document.body
+let img  = document.querySelector('.men_vaz img')
+let very = []
 
-let processoRotina = ()=>{
-    generateTask(taskName.value)
-    sec_perso.style.display = 'none'
-    taskName.value = ''
-    completeTask()
+if (bd.offsetWidth >= '600') {
+    img.src  = 'imagens/tarefa - Copia.png'
 }
 
+window.addEventListener('resize',(g)=>{
+    if (bd.offsetWidth >= '600') {
+        img.src  = 'imagens/tarefa - Copia.png'
+    } else {
+        img.src  = 'imagens/tarefa.png'
+    }
+})
+
 btn_task.addEventListener('click',()=>{
+    taskName.focus()
     sec_perso.style.display = 'block'
 })
 
@@ -30,14 +43,12 @@ btn_generate.addEventListener('click',()=>{
     
 })
 
-btn_confirmar.addEventListener('click',()=>{
-    box_error.style.display  = 'none'
-})
-
-btn_cont.addEventListener('click',()=>{
-    box_error.style.display  = 'none'
-    processoRotina()
-})
+let processoRotina = ()=>{
+    generateTask(taskName.value)
+    sec_perso.style.display = 'none'
+    taskName.value = ''
+    completeTask()
+}
 
 const generateTask = (name)=>{
     let div_task = document.createElement('div')
@@ -53,29 +64,41 @@ const generateTask = (name)=>{
     list_task.appendChild(div_task)
 }
 
-
-const verificationTask = ()=>{
-    let list = [...document.querySelector('.list_task')]
-    console.log(list)
-    if  (list.length == 0) {
-       
-    }
+function rep(a) {
+    let v = Number(0)
+    very.map((c)=>{
+        if  (a.target.nextSibling.textContent == c) {
+            v += 1 
+        }
+    })
+    return v
 }
 
-const completeTask = ()=>{
-    let check = [...document.querySelectorAll('input[type=checkbox')]
-    check.map((c)=>{
-        c.addEventListener('click',()=>{
-            c.parentNode.remove()
-            mens(c.nextSibling.textContent)
+function completeTask() {
+    let check = [...document.querySelectorAll('.list_task > div')]
+    check.map((c,p)=>{
+        c.firstChild.addEventListener('click',(e)=>{
+            console.log(e.target.nextSibling.textContent)
+            let n = rep(e)
+            if (n == 0) {
+                very.push(e.target.nextSibling.textContent)
+            }
+            c.remove()
+            mens()
         })
     })
-     
 }
 
 
 
+btn_confirmar.addEventListener('click',()=>{
+    box_error.style.display  = 'none'
+})
 
+btn_cont.addEventListener('click',()=>{
+    box_error.style.display  = 'none'
+    processoRotina()
+})
 btn_back.addEventListener('click',()=>{
     sec_perso.style.display = 'none'
 })
@@ -90,3 +113,38 @@ let mens = (n)=>{
         teste.style.display = 'none'
     }, 3000);
 }
+
+btn_home.addEventListener('click',()=>{
+    div_historico.style.display = 'none'
+    main.style.display = 'block'
+})
+
+function vert_teste(val) {
+    let n =  Number(0)
+    let li = [...document.querySelectorAll('li')]
+    li.map((l)=>{
+        if (val == l.textContent) {
+            n  += 1
+        }
+    })
+    return n
+   
+}
+
+btn_historico.addEventListener('click',()=>{
+    let completed_list =document.querySelector('.completed-list')
+    div_historico.style.display = 'flex'
+    main.style.display = 'none'
+    very.map((v)=>{
+        let n = vert_teste(v)
+        if (n == 0) {
+            let element_list = document.createElement('li')
+            element_list.innerText = v
+
+            completed_list.appendChild(element_list)
+        }
+        
+    })
+    
+})
+
